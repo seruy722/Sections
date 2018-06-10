89ad10048013271b2d002e07881893e241385d69
darth lenin <@wtf>
2018-06-10 22:28:29 +0300

    new seeders robots.txt

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -11,12 +11,61 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
-        for ($i = 1; $i <= 9; $i++) {
+        //$faker = Faker\Factory::create();
+        /*for ($i = 1; $i <= 9; $i++) {
             DB::table('category')->insert([
                 'name' => $faker->title,
                 'image' => str_random(10) . '.jpg',
             ]);
-        }
+        }*/
+        DB::table('category')->delete();
+        $category = array(
+            array(
+                'id' => '1',
+                'name' => 'Танцевальные секции',
+                'image' => 'tanc.jpg',
+            ),
+            array(
+                'id' => '2',
+                'name' => 'Спортивные секции',
+                'image' => 'sport.jpg',
+            ),
+            array(
+                'id' => '3',
+                'name' => 'Художественные школы',
+                'image' => 'hudozh.jpg',
+            ),
+            array(
+                'id' => '4',
+                'name' => 'Музыкальные школы',
+                'image' => 'muz.jpg',
+            ),
+            array(
+                'id' => '5',
+                'name' => 'Театральные школы',
+                'image' => 'teatralnyj.png',
+            ),
+            array(
+                'id' => '6',
+                'name' => 'Кружки по технике',
+                'image' => 'tehn.jpg',
+            ),
+            array(
+                'id' => '7',
+                'name' => 'Языковые кружки',
+                'image' => 'yazuk.jpg',
+            ),
+            array(
+                'id' => '8',
+                'name' => 'Кружки рукоделия',
+                'image' => 'rukod.jpg',
+            ),
+            array(
+                'id' => '9',
+                'name' => 'Образовательные кружки',
+                'image' => 'obraz.jpg',
+            ),
+        );
+        DB::table('category')->insert($category);
     }
 }
--- database/seeds/SectionsTableSeeder.php
+++ database/seeds/SectionsTableSeeder.php
@@ -12,7 +12,7 @@ class SectionsTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create();
-        for ($i = 1; $i <= 26; $i++) {
+       /* for ($i = 1; $i <= 26; $i++) {
             DB::table('sections')->insert([
                 'user_id' => rand(1, 26),
                 'section_name' => $faker->company,
@@ -21,6 +21,155 @@ class SectionsTableSeeder extends Seeder
                 'img_logo' => str_random(10) . '.jpg',
                 'info' => $faker->text($maxNbChars = 300),
             ]);
-        }
+        }*/
+        DB::table('sections')->delete();
+        $sections = array(
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок «Историки-краеведы»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '9',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Детская астрономическая обсерватория',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '9',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => '',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '1',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => '',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '1',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Хореографический ансамбль "Вітерець"',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '2',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Ансамбль спортивного бального танца «Серпантин»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '2',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок декоративно-прикладного творчества «Художественная мозайка»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '3',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок изобразительного искусства',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '3',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок самодеятельной песни',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '4',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Образцовый ансамбль эстрадной песни "Барви"',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '4',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Театр-студия «Маскарад»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '5',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Детский театр танца «Солнечная сторона»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '5',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Aвиамодельный кружок',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '6',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок телекоммуникационных технологий',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '6',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок «Английский язык»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '7',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок «Английский язык»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '7',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок «Современное вязание спицами», секция химии МАН',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '8',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+            array(
+                'user_id' => rand(1, 26),
+                'section_name' => 'Кружок «Техника рукоделия», «Бисероплетение»',
+                'address' => 'ул. Парковая, 12а, Краматорск, Донецкая область',
+                'category_id' => '8',
+                'img_logo' => 'placeholder.png',
+                'info' => $faker->text($maxNbChars = 300),
+            ),
+
+        );
+        DB::table('sections')->insert($sections);
     }
 }
